9d885f57252f4f689cd9750ace09c197da341409
Yaman Khan <khanyaman2003@gmail.com>
2026-05-07 08:49:55 +0530

    feat(frontend): integrate authentication routes into application

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,22 @@
 import { Routes, Route } from "react-router-dom";
-console.log(import.meta.env.VITE_API_BASE_URL);
+
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
 function App() {
   return (
     <Routes>
       <Route path="/" element={<h1>Home</h1>} />
-      <Route path="/login" element={<h1>Login</h1>} />
-      <Route path="/register" element={<h1>Register</h1>} />
-      <Route path="/bookmarks" element={<h1>Bookmarks</h1>} />
+
+      <Route
+        path="/login"
+        element={<Login />}
+      />
+
+      <Route
+        path="/register"
+        element={<Register />}
+      />
     </Routes>
   );
 }
